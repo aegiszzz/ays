4b2de9115c20bd6ff35a2cc12ebe2c46eeeb7723
aegiszzz <108485405+aegiszzz@users.noreply.github.com>
2025-11-24 13:41:59 +0300

    Added +html.tsx

--- app/+html.tsx
+++ app/+html.tsx
@@ -1,30 +1,27 @@
-import { Children } from 'react';
-import { AppRegistry } from 'react-native';
+import { ScrollViewStyleReset } from 'expo-router/html';
+import { type PropsWithChildren } from 'react';
 
-export default function Document() {
+export default function Root({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
         <meta name="application-name" content="Social Media PWA" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="SocialPWA" />
         <meta name="mobile-web-app-capable" content="yes" />
-
         <meta name="description" content="A decentralized social media platform with crypto wallet integration" />
         <meta name="theme-color" content="#000000" />
 
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/assets/images/favicon.png" />
-        <link rel="apple-touch-icon" href="/assets/images/icon.png" />
+
+        <ScrollViewStyleReset />
       </head>
-      <body>
-        <div id="root" />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
