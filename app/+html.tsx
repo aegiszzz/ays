ed6c7019b4a903d4bb5a21cde6dddfef8636e295
aegiszzz <108485405+aegiszzz@users.noreply.github.com>
2025-12-25 16:10:36 +0300

    Updated +html.tsx

--- app/+html.tsx
+++ app/+html.tsx
@@ -9,14 +9,16 @@ export default function Root({ children }: PropsWithChildren) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-        <meta name="application-name" content="Social Media PWA" />
+        <meta name="application-name" content="Ays" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        <meta name="apple-mobile-web-app-title" content="SocialPWA" />
+        <meta name="apple-mobile-web-app-title" content="Ays" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="description" content="A decentralized social media platform with crypto wallet integration" />
         <meta name="theme-color" content="#000000" />
 
+        <link rel="icon" type="image/png" href="/assets/images/icon.png" />
+        <link rel="apple-touch-icon" href="/assets/images/icon.png" />
         <link rel="manifest" href="/manifest.json" />
 
         <ScrollViewStyleReset />
